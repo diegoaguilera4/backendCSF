d914135cc3dc3a5e5f611bf10fe72f6aa851374a
Diego Aguilera <diegoaguilera4@icloud.com>
2024-01-16 11:06:12 -0300

    pequeños cambios en el modelo del control de desperdicio

--- src/models/controlDesperdicio.js
+++ src/models/controlDesperdicio.js
@@ -5,17 +5,19 @@ const controlDesperdicioSchema = new Schema({
     codigoDocumento: {type: String, required: true},
     nroRevision: {type: Number, required: true},
     area: {type: String, required: true},
-    fecha: {type: Date, required: true},
+    fecha: {type: Date, default: Date.now},
+    turno: {type: Number, required: true},
     responsable: {type: String, required: true},
     defectoEnLamina: {type: String, required: true},
-    causaLamina: {type: String, required: true},
+    causaLamina: {type: String},
     defectoEnCaja: {type: String, required: true},
-    causaCaja: {type: String, required: true},
+    causaCaja: {type: String},
     operadorPicadora: {type: String, required: true},
     cliente: {type: String, required: true},
     producto: {type: String, required: true},
     cantidad: {type: Number, required: true},
-    nroOP: {type: Number, required: true},
+    nroOp: {type: Number, required: true},
+    autorizaPicar: {type: String, required: true},
     totalKilos: {type: Number, required: true}
 });
 
