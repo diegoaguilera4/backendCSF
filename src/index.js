47bf6d9247ebdcf341d5a44700184a34ffc75193
Diego Aguilera <diegoaguilera4@icloud.com>
2024-01-17 12:01:16 -0300

    se crean variables otros en el control

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use('/api', controlDesperdicio);
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.send('Hello World');    
 });
 
 app.listen(port, () => {
--- src/models/controlDesperdicio.js
+++ src/models/controlDesperdicio.js
@@ -4,12 +4,15 @@ import { Schema } from "mongoose";
 const controlDesperdicioSchema = new Schema({
     nroRevision: {type: Number, required: true},
     area: {type: String, required: true},
+    areaOtra: {type: String},
     fecha: {type: Date, default: Date.now},
     turno: {type: Number, required: true},
     responsable: {type: String, required: true},
     defectoEnLamina: {type: String, required: true},
+    defectoEnLaminaOtros: {type: String},
     causaLamina: {type: String},
     defectoEnCaja: {type: String, required: true},
+    defectoEnCajaOtros: {type: String},
     causaCaja: {type: String},
     operadorPicadora: {type: String, required: true},
     cliente: {type: String, required: true},
@@ -19,6 +22,8 @@ const controlDesperdicioSchema = new Schema({
     autorizaPicar: {type: String, required: true},
     totalKilos: {type: Number, required: true}
 });
+
+
 
 const ControlDesperdicio = mongoose.model("controlDesperdicio", controlDesperdicioSchema);
 
