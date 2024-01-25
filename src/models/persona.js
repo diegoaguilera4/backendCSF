8ebd26963232ae7c23fcdebe401dc67c2e445c02
Diego Aguilera <diegoaguilera4@icloud.com>
2024-01-25 10:34:03 -0300

    se elimina arreglo autos del modelo persona

--- src/models/persona.js
+++ src/models/persona.js
@@ -5,15 +5,7 @@ const personaSchema = new Schema({
     nombre: { type: String, required: true },
     rut: { type: String, required: true, unique: true},
     d_cencos: { type: String, required: true },
-    d_cargo: { type: String, required: true },
-    autos: [
-        {
-            marca: { type: String},
-            modelo: { type: String},
-            patente: { type: String},
-            color: { type: String},
-        }
-    ],
+    d_cargo: { type: String, required: true }
 });
 
 const Persona = mongoose.model("Persona", personaSchema);
